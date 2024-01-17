ae4507c8f954bb00b8631361e135f2c09b722b9f
unknown <yehonatanshavit93@gmail.com>
2024-01-17 15:39:19 +0200

    seperate the code that runs mlflow and the code that runs copy since it is to heavy to the lambda function

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,6 @@ FROM public.ecr.aws/lambda/python:3.8
 COPY requirements.txt ./
 RUN pip install -r requirements.txt
 
-# copy .pem file
-COPY mlflow-test-ec2.pem ./
-
 # run example
 COPY full_code.py ./
 CMD ["full_code.lambda_handler"]
--- full_code.py
+++ full_code.py
@@ -1,7 +1,6 @@
 import os
 import mlflow
 import numpy as np
-from paramiko import SSHClient, AutoAddPolicy, RSAKey
 
 
 def run_mlflow_code():
@@ -39,39 +38,11 @@ def run_mlflow_code():
     return experiment_id, experiment_name, run_name, run_id
 
 
-def copy_from_ec2_to_s3():
-    """
-    copy mlruns directory from ec2 to s3 bucket
-    """
-
-    # Connect to EC2 instance
-    ssh = SSHClient()
-    ssh.set_missing_host_key_policy(AutoAddPolicy())
-    private_key = RSAKey(filename=os.environ['ec2_private_key_path'])
-    stdout = []
-
-    try:
-        # connect
-        ssh.connect(os.environ['ec2_instance_ip'], username=os.environ['ec2_username'], pkey=private_key)
-        # run linux command - copy mlruns directory from ec2 to s3
-        copy_command = "aws s3 sync {} s3://{}/{}".format(os.environ['ec2_path'],
-                                                          os.environ['s3_name'],
-                                                          os.environ['s3_path'])
-        # get output
-        _, stdout, _ = ssh.exec_command(copy_command)
-        stdout = stdout.read().decode().split("\n")
-
-    finally:
-        ssh.close()
-
-    return stdout
-
-
 def lambda_handler(event, context):
     experiment_id, experiment_name, run_name, run_id = run_mlflow_code()
-    copy_output = copy_from_ec2_to_s3()
-    return {"experiment_id": experiment_id,
-            "experiment_name": experiment_name,
-            "run_name": run_name,
-            "run_id": run_id,
-            "copy_output": copy_output}
+    return {
+        "experiment_id": experiment_id,
+        "experiment_name": experiment_name,
+        "run_name": run_name,
+        "run_id": run_id,
+    }
